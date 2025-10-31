b64f51b392f2424f9e87ecd905a70f799f17af54
z0ppi <pink.devel@proton.me>
2025-10-31 20:57:04 +0100

    build: update build configuration and gitignore for C files

--- make.bat
+++ make.bat
@@ -8,6 +8,7 @@ dist\bin\pink.exe -o src\c\out_pink.c src/pink
 
 md dist\bin
 
-cl /MP /FS /Ox /W0 /nologo src\c\pink.c /Fe:dist\bin\pink.exe /EHsc /link /SUBSYSTEM:CONSOLE /NODEFAULTLIB:msvcrt.lib /NODEFAULTLIB:LIBCMT  kernel32.lib vcruntime.lib msvcrt.lib 
+gcc -Ofast -flto -march=native -w -c src\c\pink.c -o src\c\pink.o
+gcc -Ofast -flto -march=native -w -static-libgcc -o dist\bin\pink.exe src\c\pink.o -lkernel32
 
 endlocal
